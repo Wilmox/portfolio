d7b3ed333a2667a77ea560f62aafc72da33d6086
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2021-08-24 21:30:16 +0200

    More responsiveness

--- pages/index.js
+++ pages/index.js
@@ -11,48 +11,55 @@ export default function Home() {
       <Head>
         <title>Simon Wilmots {"\u{1F680}"}</title>
         <meta name="description" content="SIMONWILMOTS PORTFOLIO WEBISTE" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/assets/img/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
+      <section>
+        <Navigation hideHomeButton />
 
-        <Navigation hideHomeButton/>
-
-        <section>
+        
           <h1 className={styles.title}>
-              Simon <br/> Wilmots
+            Simon <br /> Wilmots
           </h1>
 
           <div className={styles.info}>
             <div className={styles.arrow}>
-              <OutlinedArrow width="100%" height="100%"/>
+              <OutlinedArrow width="100%" height="100%" />
             </div>
-            <StatusText text={"Astronomy Freak"}/>
-          </div>
-        </section> 
-        <section className={styles.aboutSection}>
-          <CircleText text="About About About About About About About"/>
-          <div className={styles.about}>
-              <p>
-                I've always been eager to learn from childhood. From computers and the universe to psychology and science, I had to find out how something functions. <br/><br/>
-                I am very passionate about <span>Personal development</span> and how you can always be the best version of yourself. <br/><br/>
-              </p>
-              <details>
-                <summary>Show more</summary>
-                <p>
-                    In <span>2016</span> I started my first study in Computer Science at the Miniemeninstituut in Leuven {"\u{1F1E7}\u{1F1EA}"}. In this education I have  received the basis that will serve me well in my next studie(s).<br/><br/>
-
-                    In <span>2019</span> I started at PXL University of Applied Sciences and Arts in Hasselt. Currently, I am a last-year application development student with specifications in <span>AI & Robotics</span>.
-                </p>
-              </details>
-
+            <StatusText text={"Astronomy Freak"} />
           </div>
         </section>
-        
+        <section className={styles.aboutSection}>
+
+          <div className={styles.about}>
+            <h2>ABOUT</h2> <br />
+            <p>
+              I've always been eager to learn from childhood. From computers and the universe to psychology and science, I had to find out how something functions. <br /><br />
+              I am very passionate about <span>Personal development</span> and how you can always be the best version of yourself. <br /><br />
+            </p>
+            <details>
+              <summary>Show more</summary>
+              <p>
+                <br />
+                In <span>2016</span> I started my first study in Computer Science at the Miniemeninstituut in Leuven {"\u{1F1E7}\u{1F1EA}"}. In this education I have  received the basis that will serve me well in my next studie(s).<br /><br />
+
+                In <span>2019</span> I started at PXL University of Applied Sciences and Arts in Hasselt. Currently, I am a last-year application development student with specifications in <span>AI & Robotics</span>.
+              </p>
+            </details>
+          </div>
+          <CircleText text="About About About About About About About" />
+        </section>
+
+        {/*<section className={styles.projectSection}>
+
+
+        </section>*/}
+
       </main>
     </div>
   )
