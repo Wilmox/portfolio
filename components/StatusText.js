37552e753ba9df797c4d2a54cb563ea6b324b4d2
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2021-08-29 22:45:00 +0200

    Finished project section on homepage

--- components/StatusText.js
+++ components/StatusText.js
@@ -7,7 +7,7 @@ const StatusText = ({text="Student"}) => {
     const cycle = document.getElementById("cycle");
 
     const statusList = [
-      "Student", //\u{1F393}
+      "IT Student", //\u{1F393}
       "Computer Nerd", //\u{1F468}\u{200D}\u{1F4BB}
       "Astronomy Freak", //\u{1F30C}
       "Entrepeneur", //\u{1F468}\u{200D}\u{1F4BC}
--- pages/index.js
+++ pages/index.js
@@ -62,15 +62,28 @@ export default function Home() {
 
           <div className={styles.projects}>
             <a className={styles.projectCard}>
-              <Image src="/assets/img/project-tictactoe-minmax.jpg" width={1241} height={745} />
+              <Image src="/assets/img/project-openremote.jpg" width={1241} height={745} />
 
               <div className={styles.cardText}>
-                <h2>Project</h2>
-                <p>Minimax is a decision rule used in artificial intelligence, decision theory, game theory, statistics, and philosophy for minimizing the possible loss</p>
+                <h2>OpenRemote Energy Challenge</h2>
+                <p>Using a prediction model, the goal is to observe and make energy consumption predictions.</p>
               </div>
               <div className={styles.labels}>
-                <Chip bgColor="pink" color="red" text={"AI & Robotics"} icon={"\u{1F9E0}"} />
-                <Chip bgColor="pink" color="red" text={"AI & Robotics"} icon={"\u{1F9E0}"} />
+                <Chip bgColor="rgba(52, 73, 94,1)" text={"Research"} icon={"\u{1F50D}"} />
+                <Chip bgColor="#343A40" text={"AI & Robotics"} icon={"\u{1F916}"} />
+              </div>
+            </a>
+
+            <a className={styles.projectCard}>
+              <Image src="/assets/img/project-conecta.jpg" width={1241} height={745} />
+
+              <div className={styles.cardText}>
+                <h2>Conecta Software</h2>
+                <p>Three week internship at Conecta Software & Erasmus in Tenerife, Spain in connection with IoT & smart devices</p>
+              </div>
+              <div className={styles.labels}>
+                <Chip bgColor="rgba(52, 73, 94,1)" text={"Research"} icon={"\u{1F50D}"} />
+                <Chip bgColor="#307bc7" text={"Docker"} icon={"\u{1F40B}"}/>
               </div>
             </a>
 
@@ -78,28 +91,31 @@ export default function Home() {
               <Image src="/assets/img/project-tictactoe-minmax.jpg" width={1241} height={745} />
 
               <div className={styles.cardText}>
-                <h2>Project</h2>
-                <p>Minimax is a decision rule used in artificial intelligence, decision theory, game theory, statistics, and philosophy for minimizing the possible loss</p>
+                <h2>Tic Tac Toe AI</h2>
+                <p>Making an unbeatable Tic-Tac-Toe opponent using the MiniMax decision theory algorithm.</p>
               </div>
               <div className={styles.labels}>
-                <Chip bgColor="pink" color="red" text={"AI & Robotics"} icon={"\u{1F9E0}"} />
-                <Chip bgColor="pink" color="red" text={"AI & Robotics"} icon={"\u{1F9E0}"} />
+                <Chip bgColor="rgba(243, 156, 18, .7)" text={"Learning"} icon={"\u{1F4DA}"} />
+                <Chip bgColor="#343A40" text={"AI & Robotics"} icon={"\u{1F916}"} />
               </div>
             </a>
 
             <a className={styles.projectCard}>
-              <Image src="/assets/img/project-tictactoe-minmax.jpg" width={1241} height={745} />
+              <Image src="/assets/img/project-solar-system.jpg" width={1241} height={745} />
 
               <div className={styles.cardText}>
-                <h2>Project</h2>
-                <p>Minimax is a decision rule used in artificial intelligence, decision theory, game theory, statistics, and philosophy for minimizing the possible loss</p>
+                <h2>Solar System simulation</h2>
+                <p>A selfmade 1:1 simulation of our Solar System using the Unity 3D engine. Inspired by SpaceEngine on Steam.</p>
               </div>
               <div className={styles.labels}>
-                <Chip bgColor="pink" color="red" text={"AI & Robotics"} icon={"\u{1F9E0}"} />
-                <Chip bgColor="pink" color="red" text={"AI & Robotics"} icon={"\u{1F9E0}"} />
+                <Chip bgColor="#5b3975" text={"Space"} icon={"\u{1F30C}"} />
+                <Chip bgColor="#343A40" text={"Unity Engine"} icon={"\u{1F4BB}"} />
               </div>
             </a>
           </div>
+
+          <a className={styles.link} href="/projects">{"See more projects>"}</a>
+
         </section>
 
       </main>
