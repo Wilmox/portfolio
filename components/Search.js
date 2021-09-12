7683b6bd6f2a2037e3b6d1928c0f48cfdf4df06a
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2021-09-12 10:17:09 +0200

    Search works in code

--- components/Search.js
+++ components/Search.js
@@ -1,18 +1,19 @@
 import style from './Search.module.css';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
 export default function Search() {
+    const [input, setInput] = useState('');
 
-    const search = e => {
-        e.preventDefault();
-
+    const search = (e) => {
+        setInput(e.target.value);
         console.log("You searched", input);
     }
 
     return (
         <div className={style.search}>
                 <SearchIcon className={style.search__inputIcon}/>
-                <input type="text" placeholder="Search for a note..." onChange={e => console.log(e)} />
+                <input type="text" placeholder="Search for a note..." value={input} onChange={search} />
         </div>
     )
 }
