d0a890d59e53f9f40fd12c809d9646a1540354df
Simon Wilmots <simon.wilmots1@gmail.com>
2021-09-20 20:51:37 +0200

    Chip logging added

--- components/Chip/Chip.js
+++ components/Chip/Chip.js
@@ -6,11 +6,14 @@ const Chip = ({bgColor="gray", color="white", text, icon}) => {
         'color': color + " !important",
         'backgroundColor': bgColor + " !important",
     }
+    console.log("configured color");
     return ( 
         <div style={cssStyle} className={styles.chip}>
+            {console.log("applied color")}
             <div className={styles.chip_head}>{icon}</div>
             <div className={styles.chip_content}>{text}</div>
         </div>
+        
      );
 }
 export default Chip;
