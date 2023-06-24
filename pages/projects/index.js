3672919d741895cb983382064ef4f7a7b12d121b
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2023-06-24 22:26:45 +0200

    Latest notes are now at the top of the page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -102,6 +102,13 @@ export async function getStaticProps() {
   const allProjects = getAllProjects();
   let searchProjects = allProjects;
 
+  searchNotes.sort((a, b) => {
+    const dateA = new Date(a.data.date);
+    const dateB = new Date(b.data.date);
+    //return dateA - dateB; // Ascending
+    return dateB - dateA;   // Descending
+  });
+
   return {
     props: {
       //Here data serialising (dates, urls, ...),
